9663c856688703604de428cd3e280264e6b89203
yzhu14 <yzhu14@ncsu.edu>
2015-11-16 17:15:03 -0500

    resize global vector

--- resizeVector.cpp
+++ resizeVector.cpp
@@ -1,13 +1,17 @@
-//resizing vector
+//resizing global vector
 #include <iostream>
 #include <vector>
+std::vector<int> myvector(20);//global
 int main ()
 {
-std::vector<int> myvector;
+	myvector[0]=10;
+	myvector[1]=11;
 for (int i=1;i<10;i++) myvector.push_back(i);
-myvector.resize(5);
+myvector.resize(15);
 myvector.resize(8,10);
 myvector.resize(10);
+
+for (int i=0;i<10;i++)myvector[i]=i;
 
 std::cout<<"myvector: ";
 for (int i=0;i<myvector.size();i++) std::cout<<"\t"<<myvector[i]<<"\n";
